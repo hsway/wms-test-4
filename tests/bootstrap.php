a6183087039e301f00a8872c63c812f55d7fd182
Hank Sway <hank.sway@gmail.com>
2017-09-25 22:42:24 -0400

    added stuff

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+\VCR\VCR::configure()->setCassettePath(__DIR__ . '/mocks');
+\VCR\VCR::configure()->enableRequestMatchers(array('method', 'url', 'host'));
